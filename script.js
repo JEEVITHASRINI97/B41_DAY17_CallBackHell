9d3b3fd431430e25ca118f87d42a435ae34a3ddd
JEEVITHASRINI97 <77686700+JEEVITHASRINI97@users.noreply.github.com>
2022-11-22 23:56:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+//Targetting a element
+const countdown=document.querySelector(".countdown");
+//take a variable & set its value to 10
+let time=10;
+countdown.innerHTML=time--;
+ setTimeout (()=>{
+countdown.innerHTML=time--;
+setTimeout (()=>{
+    countdown.innerHTML=time--;
+    setTimeout (()=>{
+        countdown.innerHTML=time--;
+        setTimeout (()=>{
+            countdown.innerHTML=time--;
+            setTimeout (()=>{
+                countdown.innerHTML=time--;
+                setTimeout (()=>{
+                    countdown.innerHTML=time--;
+                    setTimeout (()=>{
+                        countdown.innerHTML=time--;
+                        setTimeout (()=>{
+                            countdown.innerHTML=time--;
+                            setTimeout (()=>{
+                                countdown.innerHTML=time--;
+                                setTimeout (()=>{
+                                    countdown.innerHTML=time--;
+                                    setTimeout (()=>{
+                                        countdown.innerHTML=time--;
+                                        countdown.innerHTML="Happy Independance Day"
+},2000);
+},2000);
+},2000);
+},2000);
+},2000);
+},2000);
+},2000);
+},2000);
+},2000);
+},2000);
+},2000);
